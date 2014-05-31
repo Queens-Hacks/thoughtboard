ed4db159d4a0d95c543a9399a59ec0e1d0a95b6c
Phil Schleihauf <uniphil@gmail.com>
2014-05-31 16:02:44 -0400

    add config import

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
+#!/usr/bin/env python
+import os
 from flask import Flask
 
 app = Flask(__name__)
+
+
+getenv = lambda *names: {v: os.environ.get(v) for v in names}
+app.config.update(**getenv(
+    'AUTH_TOKEN',
+    'CELL_NUM',
+    'MONGOLAB_URI',
+    'TWILLIO_NUM',
+))
 
 
 @app.route('/')
@@ -9,4 +20,4 @@ def home():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
