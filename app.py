e58e8b346c43994138aa0f09cbba9e0ee5d52dc7
Phil Schleihauf <uniphil@gmail.com>
2014-05-31 16:37:38 -0400

    refactor config and hook up pymongo

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python
 import os
 from flask import Flask
+from flask.ext.pymongo import PyMongo
 
-app = Flask(__name__)
 
-
-getenv = lambda *names: {v: os.environ.get(v) for v in names}
-app.config.update(**getenv(
+CONFIGS = (
     'AUTH_TOKEN',
     'CELL_NUM',
     'MONGOLAB_URI',
-    'TWILLIO_NUM',
-))
+    'TWILIO_NUM',
+)
+
+
+# Set up the app
+app = Flask(__name__)
+app.config.update(**{v: os.environ[v] for v in CONFIGS})
+app.config['MONGO_URI'] = app.config['MONGOLAB_URI']  # for flask-pymongo
+
+
+# Initialize extensions
+pymongo = PyMongo(app)
 
 
 @app.route('/')
