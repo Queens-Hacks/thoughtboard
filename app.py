76e307210e2edfd8448c4e827888d482db97c287
wzaghal <wisamzaghal@gmail.com>
2014-06-01 11:24:24 -0400

    more fixes

--- app.py
+++ app.py
@@ -255,7 +255,7 @@ def get_current_post():
 
 def update_showing():
     next_ = pymongo.db.posts.find_one({'showtime': {'$exists': False}},
-                                      sort=[('showtime', DESCENDING)])
+                                      sort=[('showtime', ASCENDING)])
     if next_ is not None:
         print('changing...')
         pymongo.db.posts.update({'_id': next_['_id']},
